7f33d02bfdb858fbb181b9bbbe5f67c1ecc65a49
Hydro998 <huynguyenquang989@gmail.com>
2024-10-23 22:00:46 +0700

    updated

--- src/app/models/Review.js
+++ src/app/models/Review.js
@@ -16,20 +16,72 @@ const Review = mongoose.model('Review', reviewSchema);
 export default Review;
 
 const loadReview = async (reviewId) => {
-    const dbSession = await Review.findOne({ _id: reviewId });
-    if (dbSession) {
+    const dbReview = await Review.findOne({ _id: reviewId });
+    if (dbReview) {
       return new Review({
-        _id: dbSession._id,
-        productId: dbSession.productId,
-        productTitle: dbSession.productTitle,
-        customerId: dbSession.customerId,
-        customerName: dbSession.customerName,
-        rating: dbSession.rating,
-        message: dbSession.message,
-        ratingStatus: dbSession.ratingStatus,
+        _id: dbReview._id,
+        productId: dbReview.productId,
+        productTitle: dbReview.productTitle,
+        customerId: dbReview.customerId,
+        customerName: dbReview.customerName,
+        rating: dbReview.rating,
+        message: dbReview.message,
+        ratingStatus: dbReview.ratingStatus,
       });
     }
     return null;
   };
 
-export {loadReview};
+const storeReview = async (review) => {
+    try {
+      const filter = { _id: review._id };
+      const replacement = {
+        _id: review._id,
+        productId: review.productId,
+        productTitle: review.productTitle,
+        customerId: review.customerId,
+        customerName: review.customerName,
+        rating: review.rating,
+        message: review.message,
+        ratingStatus: review.ratingStatus,
+      };
+
+      const options = { upsert: true, new: true };
+      const dbReview = await Review.findOneAndReplace(
+        filter,
+        replacement,
+        options
+      );
+
+      return dbReview;
+    } catch (error) {
+      console.error("Error storing session");
+      return false;
+    }
+};
+
+const getReviewsByProductId = async (productId) => {
+  try {
+    const reviews = await Review.find({ productId });
+    return reviews;
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+    throw new Error('Could not fetch reviews');
+  }
+};
+
+const deleteReviewById = async (reviewId) => {
+  try {
+    const result = await Review.findByIdAndDelete(reviewId);
+    if (!result) {
+      throw new Error('Review not found');
+    }
+    return result;
+  } catch (error) {
+    console.error('Error deleting review:', error);
+    throw new Error('Could not delete review');
+  }
+};
+
+
+export {loadReview, storeReview, getReviewsByProductId, deleteReviewById};
